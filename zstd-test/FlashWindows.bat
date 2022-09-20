547c16f994c8f54c90921169d9b540bc68cd1d3a
Meetingfate <71997026+Meetingfate@users.noreply.github.com>
2022-09-20 22:01:12 +0800

    Add files via upload

--- zstd-test/FlashWindows.bat
+++ zstd-test/FlashWindows.bat
@@ -1,25 +1,25 @@
-@echo off
-title 线刷脚本 [请勿选中窗口，否则卡住活该，卡住按右键或回车或放大缩小窗口恢复]
-:MENU
-if not exist bin\Windows (
-   echo.
-   echo.未解压！
-   echo.
-   pause
-   exit
-)
-cls
+@ECHO OFF
 
+set device_name=Redmi K30 Pro
 
-if exist super_none.zst (
-   echo.正在转换 super_none.zst
-   bin\Windows\zstd.exe --rm -d super_none.zst -o super_none.img
-   if "%ERRORLEVEL%" neq "0" (
-      echo.转换失败！
-      pause
-      exit
-   )
-)
+TITLE %device_name% 线刷
+color 3f
+
+cd /d "%~dp0"
+
+:LOGO
+CLS
+ECHO.
+ECHO.                %device_name%
+ECHO.***********************************************
+ECHO.                  ROM基本信息
+ECHO.
+ECHO.             适配机型：%device_name%
+ECHO.           
+ECHO.***********************************************
+ECHO.
+ECHO.按任意键继续...
+pause>nul
 
 if exist super.zst (
    echo.正在转换 super.zst
@@ -31,56 +31,79 @@ if exist super.zst (
    )
 )
 
-echo.
-set /p WIPE="是否需要清除数据？(y/n) "
-echo.
-echo.刷完super可能会卡一会，请耐心等待！！！
-echo.若卡在 ^<waiting for any device^> 请自行寻找驱动
-echo.
-
-rem
-bin\Windows\fastboot flash "abl" "firmware-update/abl.elf"
-bin\Windows\fastboot flash "ablbak" "firmware-update/abl.elf"
-bin\Windows\fastboot flash "aop" "firmware-update/aop.mbn"
-bin\Windows\fastboot flash "aopbak" "firmware-update/aop.mbn"
-bin\Windows\fastboot flash "bluetooth" "firmware-update/BTFM.bin"
-bin\Windows\fastboot flash "cmnlib" "firmware-update/cmnlib.mbn"
-bin\Windows\fastboot flash "cmnlibbak" "firmware-update/cmnlib.mbn"
-bin\Windows\fastboot flash "cmnlib64" "firmware-update/cmnlib64.mbn"
-bin\Windows\fastboot flash "cmnlib64bak" "firmware-update/cmnlib64.mbn"
-bin\Windows\fastboot flash "devcfg" "firmware-update/devcfg.mbn"
-bin\Windows\fastboot flash "devcfgbak" "firmware-update/devcfg.mbn"
-bin\Windows\fastboot flash "dsp" "firmware-update/dspso.bin"
-bin\Windows\fastboot flash "dtbo" "firmware-update/dtbo.img"
-bin\Windows\fastboot flash "featenabler" "firmware-update/featenabler.mbn"
-bin\Windows\fastboot flash "hyp" "firmware-update/hyp.mbn"
-bin\Windows\fastboot flash "hypbak" "firmware-update/hyp.mbn"
-bin\Windows\fastboot flash "keymaster" "firmware-update/km4.mbn"
-bin\Windows\fastboot flash "logo" "firmware-update/logo.img"
-bin\Windows\fastboot flash "modem" "firmware-update/NON-HLOS.bin"
-bin\Windows\fastboot flash "qupfw" "firmware-update/qupv3fw.elf"
-bin\Windows\fastboot flash "qupfwbak" "firmware-update/qupv3fw.elf"
-bin\Windows\fastboot flash "storsec" "firmware-update/storsec.mbn"
-bin\Windows\fastboot flash "storsecbak" "firmware-update/storsec.mbn"
-bin\Windows\fastboot flash "tz" "firmware-update/tz.mbn"
-bin\Windows\fastboot flash "tzbak" "firmware-update/tz.mbn"
-bin\Windows\fastboot flash "uefisecapp" "firmware-update/uefi_sec.mbn"
-bin\Windows\fastboot flash "vbmeta_system" "firmware-update/vbmeta_system.img"
-bin\Windows\fastboot flash "vbmeta" "firmware-update/vbmeta.img"
-bin\Windows\fastboot flash "xbl_4" "firmware-update/xbl_4.elf"
-bin\Windows\fastboot flash "xbl_5" "firmware-update/xbl_5.elf"
-bin\Windows\fastboot flash "xbl_config_4" "firmware-update/xbl_config_4.elf"
-bin\Windows\fastboot flash "xbl_config_5" "firmware-update/xbl_config_5.elf"
-bin\Windows\fastboot flash "boot" "boot.img"
-
-bin\Windows\fastboot flash "super" "super_none.img"
-
-if exist super.img bin\Windows\fastboot flash super super.img
-echo.刷完super可能会卡一会，请耐心等待！！！
-
-if /i "%WIPE%" equ "y" (
-   bin\Windows\fastboot erase metadata
-   bin\Windows\fastboot erase userdata
+:ERA
+set /p clearn="ERA:是否需要清除你的数据与内部存储？(Y/N) "
+if /i "%clearn%" == "y" (
+    goto CLS
+) else if /i "%clearn%" == "n" (
+    goto CLS
+) else (
+    goto ERA
 )
-bin\Windows\fastboot reboot
-pause
+
+:CLS
+ECHO.
+ECHO.                %device_name%
+ECHO.***********************************************
+ECHO.
+ECHO.                  刷入底层文件
+ECHO.
+ECHO.***********************************************
+ECHO.
+bin\Windows\fastboot.exe %* flash abl firmware-update\abl.elf
+bin\Windows\fastboot.exe %* flash ablbak firmware-update\abl.elf
+bin\Windows\fastboot.exe %* flash aop firmware-update\aop.mbn
+bin\Windows\fastboot.exe %* flash aopbak firmware-update\aop.mbn
+bin\Windows\fastboot.exe %* flash bluetooth firmware-update\BTFM.bin
+bin\Windows\fastboot.exe %* flash cmnlib firmware-update\cmnlib.mbn
+bin\Windows\fastboot.exe %* flash cmnlibbak firmware-update\cmnlib.mbn
+bin\Windows\fastboot.exe %* flash cmnlib64 firmware-update\cmnlib64.mbn
+bin\Windows\fastboot.exe %* flash cmnlib64bak firmware-update\cmnlib64.mbn
+bin\Windows\fastboot.exe %* flash devcfg firmware-update\devcfg.mbn
+bin\Windows\fastboot.exe %* flash devcfgbak firmware-update\devcfg.mbn
+bin\Windows\fastboot.exe %* flash dsp firmware-update\dspso.bin
+bin\Windows\fastboot.exe %* flash dtbo firmware-update\dtbo.img
+bin\Windows\fastboot.exe %* flash featenabler firmware-update\featenabler.mbn
+bin\Windows\fastboot.exe %* flash hyp firmware-update\hyp.mbn
+bin\Windows\fastboot.exe %* flash hypbak firmware-update\hyp.mbn
+bin\Windows\fastboot.exe %* flash keymaster firmware-update\km4.mbn
+bin\Windows\fastboot.exe %* flash logo firmware-update\logo.img
+bin\Windows\fastboot.exe %* flash modem firmware-update\NON-HLOS.bin
+bin\Windows\fastboot.exe %* flash qupfw firmware-update\qupv3fw.elf
+bin\Windows\fastboot.exe %* flash qupfwbak firmware-update\qupv3fw.elf
+bin\Windows\fastboot.exe %* flash storsec firmware-update\storsec.mbn
+bin\Windows\fastboot.exe %* flash storsecbak firmware-update\storsec.mbn
+bin\Windows\fastboot.exe %* flash tz firmware-update\tz.mbn
+bin\Windows\fastboot.exe %* flash tzbak firmware-update\tz.mbn
+bin\Windows\fastboot.exe %* flash uefisecapp firmware-update\uefi_sec.mbn
+bin\Windows\fastboot.exe %* flash vbmeta_system firmware-update\vbmeta_system.img
+bin\Windows\fastboot.exe %* flash vbmeta firmware-update\vbmeta.img
+bin\Windows\fastboot.exe %* flash xbl_4 firmware-update\xbl_4.elf
+bin\Windows\fastboot.exe %* flash xbl_5 firmware-update\xbl_5.elf
+bin\Windows\fastboot.exe %* flash xbl_config_4 firmware-update\xbl_config_4.elf
+bin\Windows\fastboot.exe %* flash xbl_config_5 firmware-update\xbl_config_5.elf
+bin\Windows\fastboot.exe %* flash boot boot.img
+
+@REM flash firmware done
+
+:MAIN
+
+if exist super.img bin\Windows\fastboot.exe %* flash super super.img
+echo.开始刷入super镜像，此文件较大，耗时可能较长。
+echo.刷完super可能会卡一会，请耐心等待！！！
+
+if /i "%clearn%" == "y" (
+    ECHO. 开始清除数据与内部存储
+    bin\Windows\fastboot.exe %* erase metadata
+    bin\Windows\fastboot.exe %* erase userdata
+)
+
+echo. 刷入完成(按任意键重启手机)
+pause >NUL
+bin\Windows\fastboot.exe %* reboot
+
+echo. 线刷执行完毕(按任意键关闭脚本)
+pause >NUL
+EXIT
+
+
